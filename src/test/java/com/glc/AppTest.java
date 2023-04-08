5f1b8a0c1a60494b4fe64fb33234277e30fd6fbe
Mehrunissa Pirwani (2303.KHI.CND.007) <127718818+MehrXloop@users.noreply.github.com>
2023-04-09 01:34:49 +0500

    AC 1 test failed

--- src/test/java/com/glc/AppTest.java
+++ src/test/java/com/glc/AppTest.java
@@ -17,4 +17,15 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+     //AC 1:Given that I visit the site, when I first start, I expect my list to be empty.
+
+    @Test
+    public void emptyList(){
+        //Setup
+        ReadingList readingList = new ReadingList();
+        //Assert
+        List<String> bookList = readingList.bookList;
+        assertEquals(0,bookList.size());
+    }
 }
