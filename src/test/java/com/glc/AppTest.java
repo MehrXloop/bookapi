f2efbdf26c9a7ac9639f157767b525cd1cb96221
Mehrunissa Pirwani (2303.KHI.CND.007) <127718818+MehrXloop@users.noreply.github.com>
2023-04-09 03:17:50 +0500

    AC 4 : test failed

--- src/test/java/com/glc/AppTest.java
+++ src/test/java/com/glc/AppTest.java
@@ -3,6 +3,7 @@ package com.glc;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.Test;
@@ -61,6 +62,28 @@ public class AppTest
  
          //Assert
          assertEquals(2,readingList.numberRead());
+     }
+
+     //Ac 4:Given that I have a book in my list, when I call `removeBook("<title>")` with "title" representing the title of my book that I want to delete, then when I call `getBooks()` the book I deleted should no longer be there.
+
+     @Test
+     public void removeBook(){
+        //Setup
+        Book book = new Book("The psychology of money", "Morgan Housel", 240, 1890);
+        Book book2 = new Book("Rich Dad poor Dad", "robert kiawski", 250, 1998);
+        ReadingList readingList = new ReadingList();
+
+        List<String> result = new LinkedList<>();
+        result.add("Rich dad poor dad by robert kiawski, 250 pages, 1998 read on January 4, 2023,****");
+
+        //Execute
+        readingList.addBook(book, "January 1, 2023", 5);
+        readingList.addBook(book2, "January 4, 2023", 4);
+        readingList.removeBook("The psychology of money");
+         
+        //Assert
+        assertEquals(result,readingList.getBooks());
+    
      }
 
 }
